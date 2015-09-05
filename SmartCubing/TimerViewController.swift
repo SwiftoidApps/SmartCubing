6a7bbd74aa3356fdd11977d5ff253419d85fae90
Aaron Rosenthal <Aaron@Aarons-MacBook-Pro.local>
2015-09-05 14:57:28 -0400

    Touch recognition for timer and timer functionality

--- SmartCubing/TimerViewController.swift
+++ SmartCubing/TimerViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    var timing = false
+    var startTime = NSTimeInterval()
+    var timer = NSTimer()
+    
+    @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +24,47 @@ class TimerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        if(!timing) {
+            startTimer()
+        } else {
+            stopTimer()
+        }
+    }
+    // Updates display label
+    func updateTime() {
+        if(timing) {
+        var curTime = NSDate.timeIntervalSinceReferenceDate()
+        var elapsedTime = curTime - startTime
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        var timeText = String(seconds) + "." + strFraction
+        if(minutes > 0) {
+            timeText = String(minutes) + ":" + timeText
+        }
+        timerLabel.text = timeText
+    }
+    }
+    
+    func startTimer() {
+        timing = true
+        let aSelector : Selector = "updateTime"
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+        startTime = NSDate.timeIntervalSinceReferenceDate()
+    }
+    
+    func stopTimer() {
+        timing = false
+    }
 
     /*
     // MARK: - Navigation
