a93c71f97c9aec26c466da0061fd0a8edb2d3af6
Aaron Rosenthal <Aaron@aarons-mbp.home>
2015-09-05 00:16:14 -0400

    Initial project setup

---
+++ SmartCubing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartCubing
+//
+//  Created by Aaron Rosenthal on 9/4/15.
+//  Copyright (c) 2015 Swiftoid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
